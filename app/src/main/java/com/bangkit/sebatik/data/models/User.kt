041eb22817b8ea58d66f1f60adce6a915807c1e4
Enneva <ramaadjiprsty@gmail.com>
2024-05-28 18:53:59 +0700

    adding additional data for sign up

---
+++ app/src/main/java/com/bangkit/sebatik/data/models/User.kt
@@ -0,0 +1,7 @@
+package com.bangkit.sebatik.data.models
+
+class User(
+    val username: String,
+    val email: String,
+    val phoneNumber: String
+)
--- app/src/main/java/com/bangkit/sebatik/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/bangkit/sebatik/ui/register/RegisterActivity.kt
@@ -8,15 +8,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bangkit.sebatik.R
+import com.bangkit.sebatik.data.models.User
 import com.bangkit.sebatik.databinding.ActivityLoginBinding
 import com.bangkit.sebatik.databinding.ActivityRegisterBinding
 import com.bangkit.sebatik.ui.login.LoginActivity
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,15 +30,23 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        database = FirebaseDatabase.getInstance().reference
+        firebaseAuth = Firebase.auth
+
         binding.tvToSignin.setOnClickListener {toLogin()}
         binding.btnRegister.setOnClickListener {
             val email = binding.edEmail.text.toString()
             val password = binding.edPassword.text.toString()
+            val username = binding.edUsername.text.toString()
+            val phoneNumber = binding.edPhone.text.toString()
 
             if (email.isNotEmpty() && password.isNotEmpty()) {
                 firebaseAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
+                            val userId = firebaseAuth.currentUser!!.uid
+                            val user = User(username, email, phoneNumber)
+                            database.child("users").child(userId).setValue(user)
                             Toast.makeText(this, "Signup Successful", Toast.LENGTH_SHORT).show()
                             toLogin()
                             finish()
