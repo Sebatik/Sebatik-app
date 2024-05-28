62cabbb3eb0dedf29acd356bce9ace35614b1a91
Enneva <ramaadjiprsty@gmail.com>
2024-05-29 05:38:15 +0700

    change validation at register

--- app/src/main/java/com/bangkit/sebatik/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/bangkit/sebatik/ui/register/RegisterActivity.kt
@@ -2,16 +2,9 @@ package com.bangkit.sebatik.ui.register
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Patterns.EMAIL_ADDRESS
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.bangkit.sebatik.R
 import com.bangkit.sebatik.data.models.User
-import com.bangkit.sebatik.databinding.ActivityLoginBinding
 import com.bangkit.sebatik.databinding.ActivityRegisterBinding
 import com.bangkit.sebatik.ui.login.LoginActivity
 import com.google.firebase.Firebase
@@ -41,35 +34,33 @@ class RegisterActivity : AppCompatActivity() {
             val password = binding.edPassword.text.toString()
             val username = binding.edUsername.text.toString()
             val phoneNumber = binding.edPhone.text.toString()
+            val fields = listOf(email, password, username, phoneNumber)
 
-            if (!EMAIL_ADDRESS.matcher(email).matches()) {
-                binding.edEmail.error = "Invalid Email"
-            }
-            if (TextUtils.isEmpty(password) || password.length < 6 ) {
-                binding.edUsername.error = "Invalid Password"
-            }
-            if (TextUtils.isEmpty(phoneNumber)) {
-                binding.edPhone.error = "Enter Phone Number"
-            }
-            if (TextUtils.isEmpty(username)) {
-                binding.edUsername.error = "Enter Username"
-            }
-
-            firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        val userId = firebaseAuth.currentUser!!.uid
-                        val user = User(username, email, phoneNumber)
-                        database.child("users").child(userId).setValue(user)
-                        Toast.makeText(this, "Signup Successful", Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+            if (fields.all { it.isNotEmpty() }) {
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            val userId = firebaseAuth.currentUser!!.uid
+                            val user = User(username, email, phoneNumber)
+                            database.child("users").child(userId).setValue(user)
+                            Toast.makeText(this, "Signup Successful", Toast.LENGTH_SHORT).show()
+                            toLogin()
+                        } else {
+                            val parts = task.exception.toString().split(":")
+                            val errorMessage = parts[1].trim()
+                            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+                        }
                     }
-                }
+            } else {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
     private fun toLogin() {
-        startActivity(Intent(this, LoginActivity::class.java))
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
     }
 }
